034bdc222c14e38e6749de99891b28a49980b510
David M <david.munozs@atos.net>
2018-01-14 14:32:27 +0100

    First Commit - Currency exchange service

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10001,'USD', 'EUR', 1.15,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10002,'EUR', 'USD', 0.85,0);
+
