3acc8065544cee47157c870df402ee6de03b6d35
David M <david.munozs@atos.net>
2018-01-14 14:26:39 +0100

    First commit - Zipkin tracing server

---
+++ zipkin-distributed-tracing-server/src/main/java/com/ojodev/microservices/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
@@ -0,0 +1,19 @@
+package com.ojodev.microservices.zipkindistributedtracingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin.server.EnableZipkinServer;
+
+
+
+@EnableZipkinServer
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ZipkinDistributedTracingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinDistributedTracingServerApplication.class, args);
+	}
+}
